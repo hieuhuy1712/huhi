11eedd952e04987c2281376e3423a4b68bc889cc
huyhieu <kobietdatten17124@gmail.com>
2025-09-18 15:44:45 +0700

    Add files via upload

---
+++ BT3.java
@@ -0,0 +1,12 @@
+import  java.util.*;
+public class BT3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chieu dai cua hinh chu nhat: ");
+        long a = sc.nextLong();
+        System.out.println("Nhap chieu rong cua hinh chu nhat: ");
+        long b = sc.nextLong();
+        System.out.println("Chu vi cua hinh chu nhat la: "+(a+b)*2);
+        System.out.println("Dien tich cua hinh chu nhat la: "+a*b);
+    }
+}
